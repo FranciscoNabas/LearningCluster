12f6d5ffd3f8a3d70d9c0b93ef56a44a1f119919
fenabas <fenabas@DSK-0201>
2018-07-04 18:38:31 -0300

    Inclusão impressão de pdf via Spire.PDF versão de testes

--- App1/WebApplication1/EPLDirectPrint.aspx
+++ App1/WebApplication1/EPLDirectPrint.aspx
@@ -5,6 +5,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title>Teste Impressão EPL direto do navegador</title>
+    <meta name="description" content="The description of my page" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 <body>
     <h1>Teste Impressão EPL direto do navegador</h1>
@@ -20,13 +22,19 @@
             JanelaImpressao.print();
             JanelaImpressao.close();
         }
+
     </script>
-</body>
 
     <input type="button" value="Imprimir EPL" onclick="ImprimeEpl(document.getElementById('codigoepl').value)" /><br/>
     <textarea id="codigoepl" cols="40" rows="20">A500,100,0,2,1,1,N,"TESTE"
 P1,1
-<LF>N
+N
 
-    </textarea>
+    </textarea><br/>
+    
+    <form id="NewForm" method="post" runat="server">
+    <asp:Button Text="Imprimir PDF" runat="server" OnClick="ImprimirPDF_Click"/>
+    </form>
+            
+    </body>
 </html>
--- App1/WebApplication1/EPLDirectPrint.aspx.cs
+++ App1/WebApplication1/EPLDirectPrint.aspx.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Spire.Pdf;
+using Spire.Pdf.Annotations;
+using Spire.Pdf.Widget;
+using System.Drawing.Printing;
+using System.Windows.Forms;
 
 namespace WebApplication1
 {
@@ -11,7 +16,25 @@ namespace WebApplication1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        protected void ImprimirPDF_Click(object sender, EventArgs e)
+        {
+            PrintDialog DialogoImpressao = new PrintDialog();
+            PdfDocument Documento = new PdfDocument();
+            Documento.LoadFromFile(@"C:\Users\fenabas\Desktop\CardGenPrint.pdf");
+            DialogoImpressao.AllowPrintToFile = true;
+            DialogoImpressao.AllowSomePages = true;
+            DialogoImpressao.PrinterSettings.MaximumPage = 1;
+            DialogoImpressao.PrinterSettings.MaximumPage = Documento.Pages.Count;
+
+            if (DialogoImpressao.ShowDialog() == DialogResult.OK)
+            {
+                Documento.PrintSettings.SelectPageRange(1, Documento.Pages.Count);
+                Documento.Print();
+            }
+            
         }
     }
 }
